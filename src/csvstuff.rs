44dbdd9454f694c7606d1b0d78af59a8e92a8b8e
snaapsh0t12 <snaapsh0t1@gmail.com>
2022-05-04 11:58:00 -0500

    set up the basics and renamed project to meanapi

---
+++ src/csvstuff.rs
@@ -0,0 +1,23 @@
+
+
+struct FormData {
+    team: i32,
+    matchnum: String,
+    absent: bool,
+    teamlefttarm: bool,
+    teamcollecte: bool,
+    toppre: i32,
+    bottompre: i32,
+    missedpre: i32,
+    top: i32,
+    bottom: i32,
+    missed: i32,
+    safeareausag: i32,
+    defenceplaye: String,
+    barnumberrea: String,
+    teamattempts: bool,
+    anyrobotprob: String,
+    extranotes: String,
+    driveteamrat: String,
+    password: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 #[macro_use] extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
+mod csvstuff;
 
-fn main() {
-    println!("Hello, world!");
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![index, delay])  // Just put all of the routes in here
+        .launch()
+        .await;
 }
