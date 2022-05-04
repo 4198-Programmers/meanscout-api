7377ed364a97ee538c68853d4f92a8486aabcf37
snaapsh0t12 <snaapsh0t1@gmail.com>
2022-05-04 16:12:19 -0500

    added about everything from original

--- src/csvstuff.rs
+++ src/csvstuff.rs
@@ -1,23 +1,75 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{Write, prelude::*, BufReader};
+use serde::{Serialize, Deserialize};
+use std::borrow::Cow;
+use std::fs;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct FormData<'r> {
+    pub team: Cow<'r, i64>,
+    pub matchnum: Cow<'r, str>,
+    pub absent: Cow<'r, bool>,
+    pub teamlefttarm: Cow<'r, bool>,
+    pub teamcollecte: Cow<'r, bool>,
+    pub toppre: Cow<'r, i64>,
+    pub bottompre: Cow<'r, i64>,
+    pub missedpre: Cow<'r, i64>,
+    pub top: Cow<'r, i64>,
+    pub bottom: Cow<'r, i64>,
+    pub missed: Cow<'r, i64>,
+    pub safeareausag: Cow<'r, str>,
+    pub defenceplaye: Cow<'r, str>,
+    pub barnumberrea: Cow<'r, str>,
+    pub teamattempts: Cow<'r, bool>,
+    pub anyrobotprob: Cow<'r, str>,
+    pub extranotes: Cow<'r, str>,
+    pub driveteamrat: Cow<'r, str>,
+    pub password: Cow<'r, str>,
+}
 
-struct FormData {
-    team: i32,
-    matchnum: String,
-    absent: bool,
-    teamlefttarm: bool,
-    teamcollecte: bool,
-    toppre: i32,
-    bottompre: i32,
-    missedpre: i32,
-    top: i32,
-    bottom: i32,
-    missed: i32,
-    safeareausag: i32,
-    defenceplaye: String,
-    barnumberrea: String,
-    teamattempts: bool,
-    anyrobotprob: String,
-    extranotes: String,
-    driveteamrat: String,
-    password: String,
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct FormDataTest<'r> {
+    test: Cow<'r, str>
+}
+
+/// Initializing the users and loginpage files
+fn init_files() {
+    if !file_exists("data.csv") {
+        let _userfile = File::create("data.csv");
+    }
+    return
+}
+
+/// Checks if file exists
+pub fn file_exists(file: &str) -> bool {
+    return Path::new(file).exists()
+  }
+
+pub fn append_csv(content: &str) {
+    init_files();
+    let mut file = fs::OpenOptions::new()
+      .append(true)
+      .open("data.csv")
+      .unwrap();
+    
+    let _ = writeln!(file, "{}", format!("{}", content));
+    return
+}
+
+/// Wipes data.csv
+pub fn wipe_data() {
+    let _ = fs::write("data.csv", "");
+    return
+  }
+
+pub fn read_file(path: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(&path).expect("Cannot open file");
+
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 #[macro_use] extern crate rocket;
 use rocket::tokio::time::{sleep, Duration};
+use rocket::serde::json::{Json, Value, json};
 mod csvstuff;
+use rocket::fs::NamedFile;
+use std::path::{PathBuf, Path};
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
-    sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds", seconds)
+#[post("/scouting", format="json", data="<csv>")]
+async fn scouting_post(csv: Json<csvstuff::FormData<'_>>) -> String {
+    let mut owned_string: String = "".to_owned();
+    let mut thing: String;
+    let yes = [csv.team.to_string(), csv.matchnum.to_string(), csv.absent.to_string(), csv.teamlefttarm.to_string(), csv.teamcollecte.to_string(), csv.toppre.to_string(), csv.bottompre.to_string(), csv.missedpre.to_string(), csv.top.to_string(), csv.bottom.to_string(), csv.missed.to_string(), csv.safeareausag.to_string(), csv.defenceplaye.to_string(), csv.barnumberrea.to_string(), csv.teamattempts.to_string(), csv.anyrobotprob.to_string(), csv.extranotes.to_string(), csv.driveteamrat.to_string()];
+    for i in yes.iter() {
+        thing = format!("{}, ", i);
+        if String::from(i) == csv.driveteamrat.to_string() {
+            thing = format!("{}", i)
+        }
+        owned_string.push_str(&thing)
+    }
+    csvstuff::append_csv(&owned_string); 
+    format!("{}", owned_string)
+}
+
+#[get("/scouting")]
+async fn scouting_get() -> Option<NamedFile>{
+    NamedFile::open("./data.csv").await.ok()
+}
+
+#[delete("/scouting")]
+async fn scouting_delete() -> String {
+    csvstuff::wipe_data();
+    String::from("Wiped data.csv")
 }
 
 #[rocket::main]
 async fn main() {
-    rocket::build()
-        .mount("/", routes![index, delay])  // Just put all of the routes in here
+    let _ = rocket::build()
+        .mount("/", routes![index, scouting_post, scouting_get, scouting_delete])  // Just put all of the routes in here
         .launch()
         .await;
 }
