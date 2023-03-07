4385e8b956a2a82a7f16e5ca205c345046261c54
Aiden Lee <aidenjlee9478@gmail.com>
2023-03-06 19:20:45 -0600

    Added ability to take in the grid data and convert

--- src/csvstuff.rs
+++ src/csvstuff.rs
@@ -16,24 +16,24 @@ pub struct FormData<'r> {
     pub location: Cow<'r, str>, //
     pub teamleftcommu: Cow<'r, bool>,
     pub teamcollected: Cow<'r, bool>,
-    pub autochargesta: Cow<'r, bool>,
-    pub topcubes: Cow<'r, i64>,
-    pub middlecubes: Cow<'r, i64>,
-    pub bottomcubes: Cow<'r, i64>,
-    pub missedcubes: Cow<'r, i64>,
-    pub topcones: Cow<'r, i64>,
-    pub middlecones: Cow<'r, i64>,
-    pub bottomcones: Cow<'r, i64>,
-    pub missedcones: Cow<'r, i64>,
-    pub topcube: Cow<'r, i64>,
-    pub middlecube: Cow<'r, i64>,
-    pub bottomcube: Cow<'r, i64>,
-    pub missedcube: Cow<'r, i64>,
-    pub topcone: Cow<'r, i64>,
-    pub middlecone: Cow<'r, i64>,
-    pub bottomcone: Cow<'r, i64>,
-    pub missedcone: Cow<'r, i64>,
     pub autochargesta: Cow<'r, str>,
+    pub toggletesting: Cow<'r, [GridData<'r>; 27]>,
+    // pub topcubes: Cow<'r, i64>,
+    // pub middlecubes: Cow<'r, i64>,
+    // pub bottomcubes: Cow<'r, i64>,
+    // pub missedcubes: Cow<'r, i64>,
+    // pub topcones: Cow<'r, i64>,
+    // pub middlecones: Cow<'r, i64>,
+    // pub bottomcones: Cow<'r, i64>,
+    // pub missedcones: Cow<'r, i64>,
+    // pub topcube: Cow<'r, i64>,
+    // pub middlecube: Cow<'r, i64>,
+    // pub bottomcube: Cow<'r, i64>,
+    // pub missedcube: Cow<'r, i64>,
+    // pub topcone: Cow<'r, i64>,
+    // pub middlecone: Cow<'r, i64>,
+    // pub bottomcone: Cow<'r, i64>,
+    // pub missedcone: Cow<'r, i64>,
     pub defenceplayti: Cow<'r, f64>,
     pub defensiverati: Cow<'r, i64>,
     pub teamattemptsc: Cow<'r, bool>,
@@ -83,6 +83,16 @@ pub struct PitData<'r> {
     pub whatisyourfavor: Cow<'r, str>,
     pub arethereanyothe: Cow<'r, str>,
     pub password: Cow<'r, str>,
+}
+
+/// the thingies in the thingy
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct GridData<'r> {
+    pub name: Cow<'r, str>,
+    pub value: Cow<'r, bool>,
+    // element: Option<Cow<'r, str>>,
+    // toggle: Option<Cow<'r, str>>,
 }
 
 /// Just a test struct
