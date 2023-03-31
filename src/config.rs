f8f30c16934cade9846d5c946707eea6a379ff06
Aiden Lee <aidenjlee9478@gmail.com>
2023-03-30 23:27:14 -0500

    Set up a basis for converting config to meanscout code

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,8 @@ use serde::Deserialize;
 use std::collections::HashMap;
 type JsonMap = HashMap<String, serde_json::Value>;
 use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 
 // Function for trying out new things
@@ -21,6 +23,52 @@ pub fn test() {
     }
     // println!("{:?}", res);
 }
+/// Converting config.json to json usable by meanscout
+pub fn convert_to_mean() -> std::io::Result<()> {
+    let path = "config.json";
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: JsonMap = serde_json::from_str(&data).expect("Unable to parse");
+    let (_, data) = res.get_key_value("datasets").unwrap();
+
+    let datasets: JsonMap = serde_json::from_str(&data.to_string()).expect("Unable to parse");
+    let mut i = 0; // variable to count datapoints
+    for (key, value) in datasets.iter() {
+        // println!("{}: {}", key, value);
+        
+        if value.is_object() {
+            i = 0;
+            // Going over each item in the config file
+            for dataset in value.as_object().iter() {
+                let f = fs::File::create(format!("meanscout/{}.json", key))?;
+                let mut file = OpenOptions::new().write(true).append(true).open(format!("meanscout/{}.json", key))?;
+                writeln!(file, "{{\n\"metrics\": [\n")?;
+                // println!("{:?}\n\n", dataset);
+                for item in dataset.iter() {
+                    // println!("{:?}", item);
+                    if !item.1.is_object() {
+                        println!("{:?}", item);
+                        let metric = format!("  {{\"name\": \"{}\", \"type\": {}}}, ", item.0, item.1);
+                        writeln!(file, "{}", metric)?;
+                    }
+                    else {
+                        for category_items in item.1.as_object() {
+                            // println!("{:?}", category_items);
+                            for asdf in category_items.iter() {
+                                println!("{:?}", asdf);
+                                let metric = format!("  {{\"name\": \"{}\", \"type\": {}}}, ", asdf.0, asdf.1);
+                                writeln!(file, "{}", metric)?;
+                            }
+                        }
+                    }
+                }
+
+                writeln!(file, "\n]\n}}")?;
+
+            }
+        }
+    }
+    Ok(())
+}
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
@@ -30,6 +78,7 @@ pub struct Settings {
 }
 
 impl Settings {
+    #[allow(unused)]
     pub fn new() -> Result<Self, ConfigError> {
         // let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
 
