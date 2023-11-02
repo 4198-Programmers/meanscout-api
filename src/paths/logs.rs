b84ab06c829a208ae004481681cf0e847d8a4095
Aiden <aidenjlee9478@gmail.com>
2023-11-02 18:48:54 -0500

    Made things like DEBUG or INFO in logfile get coloured

--- src/paths/logs.rs
+++ src/paths/logs.rs
@@ -9,5 +9,23 @@ pub async fn logs_get() -> impl IntoResponse {
     let mut file = File::open(config.logs_dir).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    contents
+    let contents = contents.lines().map(|line| 
+        format!("<div>{}</div>", line)
+        .replace("DEBUG", "<span style=\"color: #7289da;\">DEBUG</span>")
+        .replace("INFO", "<span style=\"color: yellow;\">INFO</span>")
+        .replace("ERROR", "<span style=\"color: red;\">ERROR</span>")
+        .replace("SUCCESS", "<span style=\"color: green;\">SUCCESS</span>")
+
+    ).collect::<String>();
+    
+    let contents = format!(
+    r#"
+    <html>
+        <body style="background-color: #1e1e2e; color: #cdd6f4">
+            {}
+        </body>
+    </html>
+    "#,
+    contents);
+    axum::response::Html(contents)
 }
